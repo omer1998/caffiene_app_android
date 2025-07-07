3c37dd6bae4bd6f0de24bcea66c83386795e2901
omer1998 <omerfaris11@gmail.com>
2025-07-08 02:12:10 +0300

    feat: implement wavy line loading indicator

---
+++ app/src/main/java/com/example/caffeine/screen/drinkDetail/LoadingIndicator.kt
@@ -0,0 +1,62 @@
+package com.example.caffeine.screen.drinkDetail
+
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.caffeine.R
+
+@Composable
+fun LoadingIndicator(modifier: Modifier = Modifier) {
+    val infiniteTransition = rememberInfiniteTransition()
+    val offset = infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = LocalConfiguration.current.screenWidthDp.toFloat(),
+        animationSpec = infiniteRepeatable(
+            repeatMode = RepeatMode.Reverse,
+            animation = tween(durationMillis = 3000, easing = LinearEasing, )
+        )
+    )
+
+    Box(
+        modifier = Modifier.fillMaxWidth(),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.wavy_line),
+            contentDescription = null,
+            modifier = modifier
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(40.dp)
+                .offset(x = offset.value.dp)
+                .background(Color.White)
+
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun PreviewLoadingIndicator() {
+    LoadingIndicator()
+}
