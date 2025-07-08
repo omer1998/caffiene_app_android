aaff07a11e0fd9bb1a4ae459db99b53dfef0fb3d
omer1998 <omerfaris11@gmail.com>
2025-07-08 05:26:25 +0300

    style: optimize the style of the texts

--- app/src/main/java/com/example/caffeine/screen/drinkType/DrinkTypeScreen.kt
+++ app/src/main/java/com/example/caffeine/screen/drinkType/DrinkTypeScreen.kt
@@ -4,21 +4,14 @@ import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.animation.core.TweenSpec
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PageSize
-import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -28,20 +21,21 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.graphics.lerp
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.caffeine.R
 import com.example.caffeine.component.AppPrimaryButton
 import com.example.caffeine.component.AppScaffold
 import com.example.caffeine.navigation.DrinkDetailRoute
 import com.example.caffeine.navigation.localNavigationController
 import com.example.caffeine.screen.home.HomeAppBar
+import com.example.caffeine.ui.theme.urbanist
 
 
 @Composable
@@ -60,13 +54,48 @@ fun DrinkTypeScreen(modifier: Modifier = Modifier) {
         horizontalPadding = 0.dp,
         appBar = { HomeAppBar(modifier = Modifier.padding(horizontal = 16.dp)) }) {
         Column(
+            modifier = modifier.fillMaxWidth(),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             Text(
-                "Good Morning", modifier = Modifier.padding(top = 16.dp)
+                modifier = Modifier
+                    .padding(top = 16.dp, start = 16.dp)
+                    .fillMaxWidth(),
+                text = "Good Morning",
+                fontWeight = FontWeight(700),
+                fontFamily = urbanist,
+                fontSize = 36.sp,
+                letterSpacing = 0.25.sp,
+                color = Color(0xFFB3B3B3),
+                textAlign = TextAlign.Start
             )
-            Text("Hamsa ☀")
-            Text("What would you like to drink today?")
+            Text(
+                "Hamsa ☀",
+                modifier = Modifier
+                    .padding(start = 16.dp)
+                    .fillMaxWidth(),
+                fontWeight = FontWeight(700),
+                fontFamily = urbanist,
+                fontSize = 36.sp,
+                letterSpacing = 0.25.sp,
+                color = Color(0xFF3B3B3B),
+                textAlign = TextAlign.Start
+            )
+            Text(
+                "What would you like to drink today?",
+                modifier = Modifier
+                    .padding(
+                        start = 16.dp,
+                        top = 4.dp
+                    )
+                    .fillMaxWidth(),
+                fontWeight = FontWeight(700),
+                fontFamily = urbanist,
+                fontSize = 16.sp,
+                letterSpacing = 0.25.sp,
+                color = Color(0xCC1F1F1F),
+                textAlign = TextAlign.Start
+            )
             HorizontalPager(
                 reverseLayout = true,
                 modifier = Modifier
