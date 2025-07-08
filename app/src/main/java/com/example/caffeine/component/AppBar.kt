a79d6bdba4158045af89aef5d06c1e809c0a8794
omer1998 <omerfaris11@gmail.com>
2025-07-08 05:32:51 +0300

    refactor: optimize app bar design

--- app/src/main/java/com/example/caffeine/component/AppBar.kt
+++ app/src/main/java/com/example/caffeine/component/AppBar.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -22,8 +23,9 @@ fun AppBar(
         verticalAlignment = Alignment.CenterVertically
     ) {
         leading()
+        Spacer(modifier = Modifier.width(12.dp))
         title.let{
-            title?.invoke(modifier.padding(start=12.dp).weight(1f))
+            title?.invoke(Modifier.weight(1f))
         }
         Spacer(modifier = Modifier.weight(1f))
         actions.let {
